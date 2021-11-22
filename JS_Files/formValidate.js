504ceedc4173058ab4b6592f9fb4986bd7fb3eec
SPSOCBryan <82923594+SPSOCBryan@users.noreply.github.com>
2021-11-22 16:14:18 +0800

    Add files via upload
    Initial commit

---
+++ JS_Files/formValidate.js
@@ -0,0 +1,11 @@
+/* The purpose of this program is to simply report on a
+   successful completition of a valid Web form. */
+
+window.onload = setForm;
+
+function setForm() {
+   document.forms[0].onsubmit = function() {
+      if (this.checkValidity()) alert("No invalid data detected. Will retain data for further testing.");
+      return false;
+   }
+}
